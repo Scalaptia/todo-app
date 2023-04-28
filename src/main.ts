e9e44cca3f7e196cd651b91a777dbdfac9a5cc51
Scalaptia <fernando@haroware.com>
2023-04-28 16:38:48 -0700

    vite/tailwind config

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './styles/main.css';
+
+const app = document.querySelector<HTMLDivElement>('#app');
+
+app!.innerHTML = /*html*/`
+  <div class="w-screen h-screen bg-slate-950">
+  </div>
+`;
